1188c4b19652bb41d3e948480e9a534930511869
gigakuma <qiang.l.x@gmail.com>
2020-08-15 17:07:36 +0800

    pypi upload scripts

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tomon_sdk",  # Replace with your own username
+    version="0.1.0",
+    author="Tomon Team",
+    author_email="qiang.l.x@gmail.com",
+    description="A simple development kit for Tomon bot.",
+    url="https://github.com/tomon-world/tomon-sdk-py",
+    packages=setuptools.find_packages(),
+    keywords=['TOMON'],
+    install_requires=[            # I get to this in a second
+        'aiohttp',
+        'websocket-client',
+        'node-events',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
